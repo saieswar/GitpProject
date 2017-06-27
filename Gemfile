2c22116553ac791d707192881e2b8e9d28ea1890
saieswar <sai.eswar786@gmail.com>
2017-06-27 17:13:43 +0530

    hook

--- Gemfile
+++ Gemfile
@@ -15,6 +15,10 @@ group :production do
 end
 # Use SCSS for stylesheets
 gem 'puma'
+
+
+#gem 'responders'
+gem 'jquery-ui-rails'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
@@ -23,7 +27,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
-
+gem 'api-ai-ruby'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- app/controllers/welocme_controller.rb
+++ app/controllers/welocme_controller.rb
@@ -1,4 +1,27 @@
 class WelocmeController < ApplicationController
+  @@client = ApiAiRuby::Client.new(:client_access_token => '94f6fbf828e44dab84b409f647a7f1e5',timeout_options: [:global, { write: 10, connect: 10, read: 10 }])    
+ 
   def index
   end
+def test
+    render json:{
+       success: true,
+       speech: "pl",
+       displayText: "pl",
+       #data: {},
+      # contextOut: [],
+        source: "SHRMS"
+     }
+
+end
+
+  def ai
+    msg = params[:msg]
+    puts "msg===================#{msg}"
+    response = @@client.text_request msg
+    puts "response-----------------------#{response}" 
+    render json:{response: response}
+  end
+
+
 end
